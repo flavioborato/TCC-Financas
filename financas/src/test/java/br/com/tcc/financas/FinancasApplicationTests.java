6e025044c4e374eac340fa43510803e782431257
Flavio Fernando Borato <toraxico@msn.com>
2024-02-24 07:49:21 -0300

    Projeto base
    Inicio de Projeto TCC Engenharia de Software na Unigran, sobre Finanças

---
+++ financas/src/test/java/br/com/tcc/financas/FinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tcc.financas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
