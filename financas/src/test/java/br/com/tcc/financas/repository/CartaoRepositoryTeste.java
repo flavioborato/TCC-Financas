1ece49a47d766caabfccef086798194de2c9fe87
Flavio Fernando Borato <toraxico@msn.com>
2024-04-23 12:14:37 -0300

    Classes Testes Repository
    Criação da classe  teste de Pessoas, e inicio da criação da classe teste Cartao

--- financas/src/test/java/br/com/tcc/financas/repository/CartaoRepositoryTeste.java
+++ financas/src/test/java/br/com/tcc/financas/repository/CartaoRepositoryTeste.java
@@ -1,42 +1,43 @@
 package br.com.tcc.financas.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.math.BigInteger;
+import java.time.LocalDate;
+import java.util.Optional;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import br.com.tcc.financas.model.Cartao;
 import br.com.tcc.financas.model.Pessoa;
 import br.com.tcc.financas.model.Senha;
+import br.com.tcc.financas.model.dados.TipoCartao;
+
 
 @SpringBootTest
 @TestInstance(Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class PessoaRepositoryTeste {
+public class CartaoRepositoryTeste {
 
+	
+	
 	@Autowired
 	PessoaRepository pessoaRepository;
-
+	@Autowired
+	CartaoRepository cartaoRepository;
 	
 	private BigInteger cpf = BigInteger.valueOf(9999);
-	private BigInteger cpf2 = BigInteger.valueOf(9999);
-	
+	private Cartao cartaoNovo;
+	private Long cartaoId;
 	
 	@BeforeAll
 	void incial() {
-		
-	}
-	
-	@Test
-	@Order(1)
-	void criaPessoaTeste() {
 		Pessoa pessoaCerate = new Pessoa();	
 		Senha senha = new Senha();
 		senha.setSenha("123456");
@@ -49,42 +50,44 @@ public class PessoaRepositoryTeste {
 	}
 	
 	@Test
-	@Order(2)
-	void findByNomeTeste() {
+	@Order(1)
+	void criaPessoaTeste() {
+		Cartao cartaoCreate = new Cartao();
 		Pessoa pessoaTeste = pessoaRepository.findByNome("PessoaTeste");
-		assertEquals(pessoaTeste.getNome(),"PessoaTeste");
-		assertEquals(pessoaTeste.getCpf(), cpf);
-		assertEquals(pessoaTeste.getNivel(),2);
-		assertNotNull(pessoaTeste.getSenha());
+		cartaoCreate.setNumero(9999);
+		cartaoCreate.setPessoa(pessoaTeste);
+		cartaoCreate.setTipo(TipoCartao.CREDITO);
+		cartaoCreate.setValidade(LocalDate.now());
+		cartaoNovo = cartaoRepository.save(cartaoCreate);
+		cartaoId = cartaoNovo.getIdcartao();
+	}
+	
+	@Test
+	@Order(2)
+	void findByNumeroTeste() {
+		
 	}
 	
 	@Test
 	@Order(3)	
 	void atualizaTeste() {
-		Pessoa pessoaAtualiza = pessoaRepository.findByNome("PessoaTeste");
-		pessoaAtualiza.setNome("PessoaTesteAlteracao");
-		pessoaAtualiza.setCpf(cpf2);
-		pessoaAtualiza.setNivel(1);
-		pessoaRepository.save(pessoaAtualiza);
-		assertEquals(pessoaAtualiza.getNome(),"PessoaTesteAlteracao");
-		assertEquals(pessoaAtualiza.getCpf(), cpf2);
-		assertEquals(pessoaAtualiza.getNivel(),1);
+		
 	}
 	
 	@Test
 	@Order(4)	
 	void deleteTeste() {
-		Pessoa pessoa = pessoaRepository.findByNome("PessoaTesteAlteracao");
-		pessoaRepository.deleteById(pessoa.getIdpessoa());
 		
-		Pessoa pessoaDel = pessoaRepository.findByNome("PessoaTesteAlteracao");
-		assertEquals(pessoaDel,null);
+		cartaoRepository.deleteById(cartaoId);
+	    Optional<Cartao> cartaoDel = cartaoRepository.findById(cartaoId);
+	    assertEquals(cartaoDel,Optional.empty());
 	}
 	
 	
 	@AfterAll
 	void finaliza() {
-		
+		Pessoa pessoa = pessoaRepository.findByNome("PessoaTeste");
+		pessoaRepository.deleteById(pessoa.getIdpessoa());
 	}
 	
 }
